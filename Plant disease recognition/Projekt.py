62502aae831a4052cab4e1de3f7c61fe585a5964
4leks4ndr4 <163046997+4leks4ndr4@users.noreply.github.com>
2024-03-11 21:20:59 +0100

    Add files via upload

---
+++ Plant disease recognition/Projekt.py
@@ -0,0 +1,49 @@
+import cv2
+
+config_file = "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+frozen_model = "frozen_inference_graph.pb"
+
+# Utworzenie modelu
+model = cv2.dnn.DetectionModel(frozen_model, config_file)
+model.setInputSize(320, 320)
+model.setInputScale([1.0/127.5])
+model.setInputMean((127.5, 127.5, 127.5))
+model.setInputSwapRB(True)
+
+font_scale = 3
+font = cv2.FONT_HERSHEY_PLAIN
+
+file_name = "labels.txt"
+with open(file_name, "rt") as fpt:
+    classLabels = fpt.read().rstrip("\n").split("\n")
+
+print(classLabels)
+
+dracena_przelana = "monstera_variegata1.mp4"
+cap = cv2.VideoCapture(dracena_przelana)
+
+if not cap.isOpened():
+    raise IOError("Nie można otworzyć filmu")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    ClassIndex, confidence, bbox = model.detect(frame, confThreshold=0.55)
+
+    print(ClassIndex)
+    if len(ClassIndex) != 0:
+        for ClassInd, conf, boxes in zip(ClassIndex, confidence, bbox):
+            if ClassInd <= 80:
+                cv2.rectangle(frame, boxes, (255, 0, 0), 2)
+                cv2.putText(frame, classLabels[ClassInd-1], (boxes[0]+10, boxes[1]+40), font, fontScale=font_scale,
+                            color=(0, 255, 0), thickness=3)
+
+    cv2.imshow("Wykrywanie obiektu", frame)
+
+    if cv2.waitKey(2) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
